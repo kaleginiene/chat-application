6cda1a17ae80cd99845621edd1bcecfeeafee402
kaleginiene <ieva.kaleginiene@gmail.com>
2021-01-22 14:58:04 +0200

    created a chat page sketch

--- src/pages/Chats/Chats.jsx
+++ src/pages/Chats/Chats.jsx
@@ -1,24 +1,50 @@
-import React from "react";
-import { Button, Section } from "../../components";
+import React, { useEffect, useState, useContext } from "react";
+import { ChatContext } from "../../contexts/ChatContext";
+import { ChatBar, ChatBubble, InputField } from "../../components";
 import * as S from "./Chats.style";
 
 function Chats() {
+  const [data, setData] = useState(); //storing data from API.
+  const senderID = useContext(ChatContext); //storing chatID data for displaying chat bubbles in the chatbox
+  const [message, setMessage] = useState(""); //storing message from an input
+
+  useEffect(() => {
+    //getting data from API
+    fetch("https://api.jsonbin.io/b/60098ea2a3d8a0580c345ecb/8", {
+      headers: {
+        "secret-key":
+          "$2b$10$QSzsWWZX0dZ9oTgYIiTRiu3aQI9EEFsc/amKTuuQ0dGl3lLeFV5ju",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [setData]);
+
   return (
     <S.Main>
-      <Section background="e3e3e3">
-        Welcome to my React boilterplate. I hope that this will bring you up to
-        speed with your development.
-      </Section>
-      <Section>
-        This boilerplate uses storybook, styled-components and React.
-      </Section>
-      <Section>
-        In this boilerplate you can find most kinds of inputs, styled loader and
-        themed style elements.
-      </Section>
-      <Section>
-        <Button color="primary">Check React Out</Button>
-      </Section>
+      <S.SideBar>
+        <S.Title>Chats</S.Title>
+        <ChatBar
+          chats={data ? data.results.chats : []}
+          users={data ? data.results.users : []}
+        />
+      </S.SideBar>
+      <S.Container>
+        {senderID.state !== null && (
+          <ChatBubble
+            chat={data.results.chats.filter(
+              (chat) => chat.user_id === senderID.state
+            )}
+          />
+        )}
+        <S.Form onSubmit={(e) => e.preventDefault()}>
+          <InputField
+            type="text"
+            placeholder="Enter your message..."
+            handleChange={(e) => setMessage(e.target.value)}
+          />
+        </S.Form>
+      </S.Container>
     </S.Main>
   );
 }
--- src/pages/Chats/Chats.style.js
+++ src/pages/Chats/Chats.style.js
@@ -1,5 +1,38 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
-  min-height: 65vh;
+  display: flex;
+
+  margin: 0 auto;
+  width: 56em;
+  min-height: 80vh;
 `;
+
+export const SideBar = styled.aside`
+  width: 35%;
+
+  border: 1px solid ${(props) => props.theme.primary.background};
+  border-right: none;
+  border-radius: 0 0 0 0.5em;
+`;
+
+export const Title = styled.h3`
+  font-size: 0.8em;
+  text-align: center;
+  font-weight: normal;
+`;
+
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+
+  padding: 1em;
+  width: 65%;
+
+  border: 1px solid ${(props) => props.theme.primary.background};
+  border-left: 1px solid ${(props) => props.theme.chat.backgroundIn};
+  border-radius: 0 0 0.5em 0;
+`;
+
+export const Form = styled.form``;
