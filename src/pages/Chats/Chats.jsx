777005ac5e2ac76ca7b2c366ca2b0d65994183aa
kaleginiene <ieva.kaleginiene@gmail.com>
2021-01-24 14:41:15 +0200

    message sending feature done

--- src/pages/Chats/Chats.jsx
+++ src/pages/Chats/Chats.jsx
@@ -3,14 +3,36 @@ import { ChatContext } from "../../contexts/ChatContext";
 import { ChatBar, ChatBubble, InputField } from "../../components";
 import * as S from "./Chats.style";
 
+function postMessage(data) {
+  //updating data with new messages into the server
+  fetch("https://api.jsonbin.io/b/60098ea2a3d8a0580c345ecb", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "secret-key":
+        "$2b$10$QSzsWWZX0dZ9oTgYIiTRiu3aQI9EEFsc/amKTuuQ0dGl3lLeFV5ju",
+      versioning: "false",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
+
 function Chats() {
   const [data, setData] = useState(); //storing data from API.
+  console.log(data);
   const senderID = useContext(ChatContext); //storing chatID data for displaying chat bubbles in the chatbox
-  const [message, setMessage] = useState(""); //storing message from an input
+  const [message, setMessage] = useState({
+    id: null,
+    user_id: "user",
+    text: "",
+  }); //storing message from an input
 
   useEffect(() => {
     //getting data from API
-    fetch("https://api.jsonbin.io/b/60098ea2a3d8a0580c345ecb/8", {
+    fetch("https://api.jsonbin.io/b/60098ea2a3d8a0580c345ecb/9", {
       headers: {
         "secret-key":
           "$2b$10$QSzsWWZX0dZ9oTgYIiTRiu3aQI9EEFsc/amKTuuQ0dGl3lLeFV5ju",
@@ -37,13 +59,39 @@ function Chats() {
             )}
           />
         )}
-        <S.Form onSubmit={(e) => e.preventDefault()}>
-          <InputField
-            type="text"
-            placeholder="Enter your message..."
-            handleChange={(e) => setMessage(e.target.value)}
-          />
-        </S.Form>
+        {senderID.state !== null && (
+          <S.Form
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (message.text.length > 0) {
+                const theChat = data.results.chats.filter(
+                  //filtering chat by userID from API data
+                  (chat) => chat.user_id === senderID.state
+                );
+                theChat[0].messages.push(message);
+                setMessage({ ...message, id: null, text: "" });
+                postMessage(data);
+                e.target.reset();
+              }
+            }}
+          >
+            <InputField
+              type="text"
+              placeholder="Enter your message..."
+              handleChange={(e) => {
+                const theChat = data.results.chats.filter(
+                  //filtering chat by userID from API data
+                  (chat) => chat.user_id === senderID.state
+                );
+                setMessage({
+                  ...message,
+                  id: theChat[0].messages.length + 1,
+                  text: e.target.value,
+                });
+              }}
+            />
+          </S.Form>
+        )}
       </S.Container>
     </S.Main>
   );
