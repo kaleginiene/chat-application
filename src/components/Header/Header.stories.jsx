94391176faa9d0182e055a09a4433c2283f69f9f
kaleginiene <ieva.kaleginiene@gmail.com>
2021-01-26 14:50:02 +0200

    removed useless components

--- src/components/Header/Header.stories.jsx
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from "@storybook/react";
-import StoryRouter from "storybook-react-router";
-import React from "react";
-import Header from "./Header";
-
-storiesOf("Header", module)
-  .addDecorator(StoryRouter())
-  .add("Header", () => <Header />);
