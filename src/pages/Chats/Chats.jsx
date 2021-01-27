5379d0b29042998e34e714eb07d901434227399e
kaleginiene <ieva.kaleginiene@gmail.com>
2021-01-27 10:59:10 +0200

    Added a ProfileBlock, validation for Chat bubbles, SideBar chat box validation. Scroll bugs not fixed yet

--- src/pages/Chats/Chats.jsx
+++ src/pages/Chats/Chats.jsx
@@ -1,7 +1,22 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
+import { useHistory } from "react-router-dom";
 import { ChatContext } from "../../contexts/ChatContext";
-import { ChatBar, ChatBubble, InputField } from "../../components";
+import { UserContext } from "../../contexts/UserContext";
+import {
+  Button,
+  ChatBar,
+  ChatBubble,
+  InputField,
+  ProfileBlock,
+} from "../../components";
 import * as S from "./Chats.style";
+import {
+  DefaultPhoto,
+  EditIcon,
+  SendIcon,
+  SendIconMobile,
+  ChattingPeople,
+} from "../../assets";
 
 function postMessage(data) {
   //updating data with new messages into the server
@@ -20,15 +35,57 @@ function postMessage(data) {
     .catch((err) => console.log(err));
 }
 
+function setMessageID(data, senderID) {
+  //setting the message id for the new chats
+  const theChat = data.results.chats.filter(
+    //filtering messages by userID from API data
+    (chat) => chat.user_id === senderID.state
+  );
+  if (theChat.length === 1) {
+    //if there were a chat with the user, then id of the new message is going to be messages quantity + 1
+    const newMsgID = theChat[0].messages.length + 1;
+    console.log("additional message");
+    return newMsgID;
+  } else {
+    //if there were no messages with the user, then for the first message setting the id 1
+    console.log("new message");
+    console.log(theChat);
+    return 1;
+  }
+}
+
+function createNewChat(data, setData, message, senderID) {
+  const chats = data.results.chats;
+  const newChatID = chats.length + 1;
+  const newChat = {
+    id: newChatID,
+    user_id: senderID.state,
+    messages: [],
+  };
+  if (message.id !== null && message.text.length > 0) {
+    newChat.messages.push(message);
+    chats.push(newChat);
+    setData(data);
+  } else {
+    return chats;
+  }
+}
+
 function Chats() {
+  const history = useHistory();
   const [data, setData] = useState(); //storing data from API.
-  console.log(data);
   const senderID = useContext(ChatContext); //storing chatID data for displaying chat bubbles in the chatbox
+  const userInfo = useContext(UserContext);
+  console.log(data);
   const [message, setMessage] = useState({
     id: null,
     user_id: "user",
     text: "",
   }); //storing message from an input
+  const scrollRef = useRef();
+
+  console.log(senderID.state);
+  console.log(message);
 
   useEffect(() => {
     //getting data from API
@@ -45,54 +102,78 @@ function Chats() {
   return (
     <S.Main>
       <S.SideBar>
-        <S.Title>Chats</S.Title>
+        <ProfileBlock
+          editUrl={EditIcon}
+          handleClick={() => history.push("/profile")}
+          imgUrl={userInfo.state.image || DefaultPhoto}
+          name={userInfo.state.name + " " + userInfo.state.surname}
+          city={userInfo.state.city}
+        />
+        <S.Title>Conversations</S.Title>
         <ChatBar
           chats={data ? data.results.chats : []}
           users={data ? data.results.users : []}
         />
       </S.SideBar>
-      <S.Container>
-        {senderID.state !== null && (
-          <ChatBubble
-            chat={data.results.chats.filter(
-              (chat) => chat.user_id === senderID.state
+      <S.Wrapper>
+        <S.Container display={senderID.state}>
+          {!senderID.state && (
+            <S.Block>
+              <S.Picture src={ChattingPeople} alt="Choose the chat" />
+              <S.Title>Choose the person to chat with!</S.Title>
+            </S.Block>
+          )}
+          {senderID.state !== null &&
+          data && ( //if there is userID, then show the chat where the userID is included
+              <ChatBubble
+                chat={data.results.chats.filter(
+                  (chat) => chat.user_id === senderID.state
+                )}
+              />
             )}
-          />
-        )}
-        {senderID.state !== null && (
-          <S.Form
-            onSubmit={(e) => {
-              e.preventDefault();
-              if (message.text.length > 0) {
-                const theChat = data.results.chats.filter(
-                  //filtering chat by userID from API data
-                  (chat) => chat.user_id === senderID.state
-                );
-                theChat[0].messages.push(message);
-                setMessage({ ...message, id: null, text: "" });
-                postMessage(data);
-                e.target.reset();
-              }
-            }}
-          >
-            <InputField
-              type="text"
-              placeholder="Enter your message..."
-              handleChange={(e) => {
-                const theChat = data.results.chats.filter(
-                  //filtering chat by userID from API data
-                  (chat) => chat.user_id === senderID.state
-                );
-                setMessage({
-                  ...message,
-                  id: theChat[0].messages.length + 1,
-                  text: e.target.value,
-                });
+          <span ref={scrollRef}></span>
+
+          {senderID.state !== null && ( //if there is userID, then show the message input
+            <S.Form
+              onSubmit={(e) => {
+                e.preventDefault();
+                if (message.text.length > 0) {
+                  const theChat = data.results.chats.filter(
+                    //filtering chat by userID from API data
+                    (chat) => chat.user_id === senderID.state
+                  );
+                  if (theChat.length === 1) {
+                    theChat[0].messages.push(message);
+                  } else {
+                    createNewChat(data, setData, message, senderID);
+                    console.log("message sent");
+                  }
+                  postMessage(data);
+                  scrollRef.current.scrollIntoView({ behavior: "smooth" });
+                  setMessage({ ...message, id: null, text: "" });
+                  e.target.reset();
+                }
               }}
-            />
-          </S.Form>
-        )}
-      </S.Container>
+            >
+              <InputField
+                type="text"
+                placeholder="Type your message here..."
+                handleChange={(e) => {
+                  setMessage({
+                    ...message,
+                    id: setMessageID(data, senderID),
+                    text: e.target.value,
+                  });
+                }}
+              />
+              <Button type="submit">
+                Send <S.SendIcon src={SendIcon} />
+              </Button>
+              <S.SendIcon className="mobile" src={SendIconMobile} />
+            </S.Form>
+          )}
+        </S.Container>
+      </S.Wrapper>
     </S.Main>
   );
 }
--- src/pages/Chats/Chats.style.js
+++ src/pages/Chats/Chats.style.js
@@ -4,35 +4,142 @@ export const Main = styled.main`
   display: flex;
 
   margin: 0 auto;
+  margin-top: 2em;
+  padding: 2em;
   width: 56em;
-  min-height: 80vh;
+  max-width: 100%;
+  height: 75vh;
+  box-shadow: -4px 7px 15px 1px rgba(0, 0, 0, 0.2);
+
+  background-color: #fff;
+  border-radius: 1em;
+
+  @media only screen and (max-width: 767px) {
+    display: block;
+
+    margin-top: 0;
+    padding: 0.5em;
+    width: 100%;
+    height: fit-content;
+    box-sizing: border-box;
+  }
 `;
 
 export const SideBar = styled.aside`
-  width: 35%;
+  overflow: auto;
 
-  border: 1px solid ${(props) => props.theme.primary.background};
-  border-right: none;
-  border-radius: 0 0 0 0.5em;
+  padding-right: 2em;
+  width: 35%;
+  height: 75vh;
+  box-sizing: border-box;
+
+  @media only screen and (max-width: 767px) {
+    padding: 0;
+    width: 100%;
+    height: auto;
+  }
 `;
 
 export const Title = styled.h3`
-  font-size: 0.8em;
+  font-size: 1em;
   text-align: center;
-  font-weight: normal;
+  font-weight: bold;
 `;
 
-export const Container = styled.section`
+export const Wrapper = styled.section`
+  width: 65%;
+  height: 75vh;
+  box-sizing: border-box;
+  position: relative;
+
+  @media only screen and (max-width: 767px) {
+    width: 100%;
+  }
+`;
+
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
 
   padding: 1em;
-  width: 65%;
+  width: 100%;
+  height: 75vh;
+  box-sizing: border-box;
+  overflow-y: scroll;
 
-  border: 1px solid ${(props) => props.theme.primary.background};
-  border-left: 1px solid ${(props) => props.theme.chat.backgroundIn};
-  border-radius: 0 0 0.5em 0;
+  background-color: ${(props) => props.theme.primary.background};
+  border-radius: 1em;
+
+  @media only screen and (max-width: 767px) {
+    display: ${(props) => (props.display !== null ? "block" : "none")};
+
+    height: fit-content;
+  }
 `;
 
-export const Form = styled.form``;
+export const Block = styled.div``;
+
+export const Picture = styled.img`
+  width: 100%;
+
+  object-fit: cover;
+
+  border-radius: 1em;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 0em 1em;
+
+  background-color: #fff;
+  border-radius: 1em;
+  box-shadow: -4px 7px 15px 1px rgba(0, 0, 0, 0.2);
+
+  input {
+    margin-bottom: 0;
+    box-sizing: border-box;
+
+    border: 2px solid #cbdaef;
+
+    @media only screen and (max-width: 767px) {
+      margin: 0;
+    }
+  }
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-left: 0.5em;
+    padding: 0.65em 2em;
+    width: 25%;
+
+    @media only screen and (max-width: 767px) {
+      display: none;
+    }
+  }
+  label {
+    margin: 0;
+  }
+`;
+
+export const SendIcon = styled.img`
+  margin-left: 0.5em;
+  height: 1em;
+
+  object-fit: cover;
+
+  &&.mobile {
+    display: none;
+
+    @media only screen and (max-width: 767px) {
+      display: block;
+      width: 1.5em;
+      height: 1.5em;
+    }
+  }
+`;
